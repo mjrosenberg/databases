d07ab53704a31afaa9edb4865c0fddab5b0f9c67
mjrosenberg <mjr70@duke.edu>
2020-06-03 17:16:06 -0700

    implemented our schema and copied client files

--- server/schema.sql
+++ server/schema.sql
@@ -2,8 +2,25 @@ CREATE DATABASE chat;
 
 USE chat;
 
+CREATE TABLE users (
+  /* Describe your table here.*/
+  id int NOT NULL,
+  user_name varchar(255),
+  PRIMARY KEY(id)
+);
+CREATE TABLE rooms (
+  /* Describe your table here.*/
+  id int NOT NULL,
+  room_name varchar(255),
+  PRIMARY KEY(id)
+);
 CREATE TABLE messages (
   /* Describe your table here.*/
+  id int NOT NULL,
+  content varchar(255),
+  user_id int NOT NULL REFERENCES users(id),
+  room_id int NOT NULL REFERENCES rooms(id),
+  PRIMARY KEY(id)
 );
 
 /* Create other tables and define schemas for them here! */
